bb304858e04530fa5717f47d264148dba434e74e
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-03-13 09:42:32 +0200

    feat: connect db to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,6 +7,17 @@ const app = express();
 
 const PORT = process.env.APP_PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is running at port ${PORT}...`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL!);
+    console.log("DB connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`server is running at port ${PORT}...`);
+    });
+  } catch (ex) {
+    console.error(ex);
+  }
+};
+
+start();
